039d66d6ab6604645a4c0e2cbc12e0a658f02d13
evanpy <evanpurnama5@gmail.com>
2012-09-09 14:51:39 +0800

    push auto

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -23,6 +23,7 @@ class SiteController < ApplicationController
 
     unless Apps.exists?(:name => apps_name)
         a = Apps.new(:name => apps_name)
+        a.user_id = user_name
         a.save
     end
 
