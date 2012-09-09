94a24ebfdd5b988d032b7fd76ab105f756a936ef
evanpy <evanpurnama5@gmail.com>
2012-09-10 07:15:39 +0800

    push auto

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -24,6 +24,7 @@ class SiteController < ApplicationController
 #    if Apps.exists?(:name => apps_name) then
 #        redirect_to "/site/index" and return
 #    end
+
     a = Apps.new(:name => apps_name)
     a.user_id = User.where(:username => user_name).first.id
     a.save
