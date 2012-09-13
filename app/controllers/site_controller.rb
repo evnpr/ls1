578f33cfd58141d3d34824dbe13f8a461b5e09ea
evanpy <evanpurnama5@gmail.com>
2012-09-13 19:02:30 +0800

    push auto

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -111,7 +111,7 @@ class SiteController < ApplicationController
         @contents = file.read
     end
     @path = path
-    unless request.GET['e'].nil?
+    unless !request.GET['e'].nil?
         render :layout => 'editor'
     end
   end
