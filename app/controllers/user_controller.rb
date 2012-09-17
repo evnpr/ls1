8ee6ad70e6330d6189dda0519979df3679b8c83a
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-17 05:09:02 +0000

    just edit /ls1/app/controllers/user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,29 @@
 class UserController < ApplicationController
-  def index
-  end
 
+  before_filter :get_users
+  
+  def get_users
+    @username = session[:username] 
+  end
+  
+  
   def login
+    if request.post?
+        username = params[:username]
+        pwd = params[:pwd]
+        session[:username] = username
+        redirect_to "/" and return
+    else
+        if session[:username]
+            redirect_to "/" and return
+        end
+    end
+  end
+  
+  def index
   end
 
   def register
   end
 end
+
