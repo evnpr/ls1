8697f646c3c8914e094baafbf8e6c45f5093568e
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-17 09:30:31 +0000

    just edit /ls1/config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Letspan::Application.routes.draw do
   get "user/login"
   post "user/login"
   get "user/register"
+  post "user/register"
   get "user/logout"
 
   get "site/index"
@@ -88,6 +89,7 @@ Letspan::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
 
 
 
