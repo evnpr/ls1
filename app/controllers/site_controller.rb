3ecd6500b88dacf28ce120a5fe776a908ffcb888
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-17 08:09:28 +0000

    just edit /ls1/app/controllers/site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -75,7 +75,7 @@ class SiteController < ApplicationController
 
   def list
     if(request.GET[:r].nil? || request.GET[:r]=='') then
-        redirect_to "/" and return
+        redirect_to "/user/index" and return
         @listfolder = Dir.glob("#{@@directory}/*/").sort
         if session[:ls1].nil?
             @listfolder = @listfolder - ["#{@@directory}/ls1/"]
@@ -346,6 +346,7 @@ class SiteController < ApplicationController
   end
 
 end
+
 
 
 
