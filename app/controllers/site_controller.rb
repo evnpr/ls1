4e05cde53c02053c48f8dfdce231ed95061458fc
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-17 11:58:24 +0000

    just edit /ls1/app/controllers/site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -282,7 +282,7 @@ class SiteController < ApplicationController
     if request.post?
         user_name = @username
         publickey = params[:key]
-        if user_name.nil? or publickey.nil?
+        if user_name.nil? or publickey == ''
             redirect_to "/" and return
         end
         temprorary_res = '/var/www/ls/res/gitosis-admin/keydir/'+user_name+'.pub'
