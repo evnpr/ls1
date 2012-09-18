49824b4afee9a7895187a025ee7d65efce3fa5a3
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-18 23:50:26 +0000

    just edit /ls1/app/controllers/user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -34,7 +34,7 @@ class UserController < ApplicationController
     end
     if User.exists?(:username => @username)
         apps = User.where(:username => @username).first.appss.all(:order => 'name')
-        if !apps.nil?
+        if apps
             @apps = apps
         end
     end
