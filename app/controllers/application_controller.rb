d6f19e4d8360979021c09c7e6003052274633a18
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-20 01:18:30 +0000

    just edit /ls1/app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
 
     AWS::S3::DEFAULT_HOST.replace "s3-ap-southeast-1.amazonaws.com"
 
+    def unzip_file (file, destination)
+      Zip::ZipFile.open(file) { |zip_file|
+       zip_file.each { |f|
+         f_path=File.join(destination, f.name)
+         FileUtils.mkdir_p(File.dirname(f_path))
+         zip_file.extract(f, f_path) unless File.exist?(f_path)
+       }
+      }
+    end
 end
+
