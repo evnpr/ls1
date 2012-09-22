5cad7c036133e77acd6f4b6f015ca7eebffd4ef7
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-22 16:44:51 +0000

    evnpr just edit /ls1/app/controllers/site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -777,7 +777,7 @@ class SiteController < ApplicationController
             redirect_to "/list?r="+params[:r] and return
         end
         flash[:list] = "you added #{colname} as collaborator of #{@apps_name}"
-        `sudo chmod -R 777 /var/www/ls/res/gitosis-admin`
+
         Dir.chdir("/var/www/ls/res/gitosis-admin"){
             `sudo sed -i 's/#{@apps_name}\ ls /#{@apps_name}\ ls\ #{colname}@#{colname} /g' /var/www/ls/res/gitosis-admin/gitosis.conf`
             `ruby push.rb`
