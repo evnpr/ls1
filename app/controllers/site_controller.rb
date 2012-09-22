d2e9c21b51c2c5bd00ee98357d1c2c71382ccf71
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-22 01:30:33 +0000

    just edit /ls1/app/controllers/site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -510,7 +510,7 @@ class SiteController < ApplicationController
           end
       end
       Dir.chdir(@@directory+"/"+apps_name){
-        `sudo chmod -R 775 .` 
+        `sudo chmod -R 755 .` 
         `git add . -A`
         `git commit -m 'upload file #{uploaded_files}'`
         `git push lsorigin2 master -f`
