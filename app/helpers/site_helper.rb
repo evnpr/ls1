7a26c3fc8ba8eb2362640e7cc24d25e48237cdb6
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-27 04:00:02 +0000

    evnpr just edit /ls1/app/helpers/site_helper.rb

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -9,9 +9,6 @@ module SiteHelper
         temppath = []
         finalpath = []
         for i in rsplit
-            if i == ''
-                next
-            end
             temppath << i
             path = temppath.join("-__-")
             finalpath << "<a href='/list/?r=#{path}'>#{i}</a>"
