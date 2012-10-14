7245c0628c52a1705c464db62b166cbe161f3549
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-14 15:15:20 +0000

    evnpr just edit /ls1/app/controllers/server_controller.rb

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -5,12 +5,21 @@ class ServerController < ApplicationController
     @username = cookies[:username] 
   end
 
+  @@directory = "/var/www/ls/upload"
 
   def index
         @apps = User.where(:username => @username).first.appss
   end
   
   def submit
+        devserver = params[:devserver]
+        prodserver = params[:prodserver]
+        appsname = params[:apps_name]
+        
+        app = Apps.where(:name => appsname).first
+        app.server.devserver = devserver
+        app.server.prodserver = prodserver
+        app.save
   end
 
 
