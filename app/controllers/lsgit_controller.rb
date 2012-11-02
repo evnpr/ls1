c824d162231daabb562c47ad40c2dfa7560c4139
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 12:28:01 +0000

    yudhoahmad just edit /ls1/app/views/site/showcontent.html.erb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -9,19 +9,22 @@ class LsgitController < ApplicationController
     unless @username
         redirect_to "/"
     end
-    username = params[:username]
-    r = params[:thisfile]
-    apps_name = r.split("-__-")[1]
-    @apps_name = apps_name
-    authenticate(Apps.where(:name => @apps_name).first.user.username, 
-                Apps.where(:name => @apps_name).first.id, 
-                User.where(:username => @username).first.id, 
-                @username)
-    user_id = User.where(:username=>username).first.id
-    n = Notifread.where(:user_id => user_id)
-    @listNotif = n
-    
-    n.destroy
+
+    r = params[:path]
+  #  apps_name = r.split("-__-")[1]
+  #  @apps_name = apps_name
+  #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
+  #              Apps.where(:name => @apps_name).first.id, 
+  #              User.where(:username => @username).first.id, 
+  #              @username)
+
+  #  user_id = User.where(:username=>@username).first.id
+  #  n = Notifread.where(:user_id => user_id)
+  #  @listNotif = n
+  #  
+  #  n.destroy
+  #
+    @r = r
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,11 @@
   
   post "server/submit"
   post "server/submitdb"
+
+
+  post "lsgit/index"
   
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
