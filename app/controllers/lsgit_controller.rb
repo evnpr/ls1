670e4e8fa3ca102aa044656223b92e598aa62780
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 10:32:15 +0000

    evnpr just edit /ls1/app/controllers/lsgit_controller.rb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -9,15 +9,20 @@ class LsgitController < ApplicationController
     unless @username
         redirect_to "/"
     end
+    username = params[:username]
     r = params[:thisfile]
-    commit = params[:commit]
     apps_name = r.split("-__-")[1]
     @apps_name = apps_name
     authenticate(Apps.where(:name => @apps_name).first.user.username, 
                 Apps.where(:name => @apps_name).first.id, 
                 User.where(:username => @username).first.id, 
                 @username)
-    path = r.gsub(/\-\_\_\-/, "\/")
+    user_id = User.where(:username=>username).first.id
+    n = Notifread.where(:user_id => user_id)
+    @listNotif = n
+    
+    n.destroy
+    
   end
 end
 
