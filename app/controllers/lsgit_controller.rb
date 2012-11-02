9b903277ab42b4ece9614628ae7a8c226ea78a3a
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 14:22:08 +0000

    evnpr just edit /ls1/app/controllers/lsgit_controller.rb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -8,8 +8,9 @@ class LsgitController < ApplicationController
   def index
     
 
-
-    @apps_name = 'ls1'
+    r = params[:path]
+    apps_name = r.split("-__-")[1]
+    @apps_name = apps_name
   #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
   #              Apps.where(:name => @apps_name).first.id, 
   #              User.where(:username => @username).first.id, 
