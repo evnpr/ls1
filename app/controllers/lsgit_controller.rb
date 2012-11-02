0e64fca272a9a5661c61e94d2ac5a51aaaf60ebc
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 10:14:36 +0000

    evnpr just edit /ls1/app/controllers/lsgit_controller.rb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -1,4 +1,23 @@
 class LsgitController < ApplicationController
+  before_filter :get_users
+  include SiteHelper
+  def get_users
+    @username = cookies[:username] 
+  end
+  
   def index
+    unless @username
+        redirect_to "/"
+    end
+    r = params[:thisfile]
+    commit = params[:commit]
+    apps_name = r.split("-__-")[1]
+    @apps_name = apps_name
+    authenticate(Apps.where(:name => @apps_name).first.user.username, 
+                Apps.where(:name => @apps_name).first.id, 
+                User.where(:username => @username).first.id, 
+                @username)
+    path = r.gsub(/\-\_\_\-/, "\/")
   end
 end
+
