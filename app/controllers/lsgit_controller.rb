4bcf116e1b879ff160f2da246b1396d3edc5bc35
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 13:42:09 +0000

    evnpr just edit /ls1/app/controllers/lsgit_controller.rb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -9,27 +9,28 @@ class LsgitController < ApplicationController
     
 
     r = params[:path]
- #   apps_name = r.split("-__-")[1]
+  #  apps_name = r.split("-__-")[1]
   #  @apps_name = apps_name
   #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
   #              Apps.where(:name => @apps_name).first.id, 
   #              User.where(:username => @username).first.id, 
   #              @username)
 
-    user_id = User.where(:username=>@username).first.id
-    n = NotifsUsers.where(:user_id => user_id)
+  # user_id = User.where(:username=>@username).first.id
+  # n = NotifsUsers.where(:user_id => user_id)
 
 
-    @listNotif = Notif.where(:notif_id => n.notif_id)
+  #   @listNotif = Notif.where(:notif_id => n.notif_id)
     
   
     @r = r
-
+    render :nothing => true
     render :layout => false
 
     
   end
 end
+
 
 
 
