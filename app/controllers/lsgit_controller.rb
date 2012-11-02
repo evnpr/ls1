60309072986b155e4aebe595d340a66cf4ceed89
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-02 16:58:46 +0000

    evnpr just edit /ls1/app/controllers/lsgit_controller.rb

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -21,9 +21,6 @@ class LsgitController < ApplicationController
     if NotifsUsers.exists?(:user_id => user_id)
         listNotifAll = Apps.find(apps_id).notifs
         listNotif = User.where(:username => @username).first.notifs
-        @listNotif = listNotif & listNotifAll
-        nu = NotifsUsers.where(:user_id => user_id)
-        nu.destroy_all
         render :json => @listNotif.to_json and return
     else
         return 
