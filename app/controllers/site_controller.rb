84d483907fc9a5823e083e352332deaa37084afa
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-05 00:16:50 +0000

    evnpr just edit /ls1/app/controllers/site_controller.rb

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -242,12 +242,10 @@ class SiteController < ApplicationController
     end
     @done = 'done rsync!'
     @path = splitpath(r)
-    @notifs = Apps.where(:name => @apps_name).first.notifs.limit(50).order("id DESC")
-    if request.GET[:notif] == 'notme'
+    
             notifs = Apps.where(:name => @apps_name).first.notifs.limit(50).order("id DESC")
             notifme = User.where(:username => @username).first.notifs.limit(50).order("id DESC")
             @notifs = notifs - notifme
-    end
     render :layout => 'editor'
   end
 
@@ -953,6 +951,7 @@ class SiteController < ApplicationController
   end
 
 end
+
 
 
 
