1264b654ff0d6b33b50c996251c24cc94119e0c4
evanpurnama <evanpurnama5@gmail.com>
2012-11-10 14:00:22 +0800

    push auto

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -105,7 +105,7 @@ class LsgitController < ApplicationController
         notifs.destroy_all
 
         bCD = true #beforeCommitDescription
-        contentReverse.each do |c|
+        contentReverse.last(100).each do |c|
             if bCD == true 
                 if c =~ /^\s*$/ 
                     bCD = false 
