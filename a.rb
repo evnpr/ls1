e9bb44418ca3d54800091b13d1207fa125229d91
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-10 04:51:58 +0000

    evnpr just edit /ls1/a.rb

--- a.rb
+++ a.rb
@@ -2,7 +2,6 @@ content = File.read('loggit')
 bCD = false #beforeCommitDescription
 u = 0
 i = 0
-
 content.each_line do |c|
     i = i + 1
     if bCD == true 
@@ -35,4 +34,5 @@ end
 
 puts u
 puts i
+
 
