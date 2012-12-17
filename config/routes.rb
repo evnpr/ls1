1a95ba42915d92eb076188eb8187eeebfdba0daa
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-12-17 22:56:03 +0000

    evnpr just edit /ls1/config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,8 @@ Letspan::Application.routes.draw do
   get "lsgit/gitToDB"
   get "lsgit/goToVersion"
   
+  get "lsgit/mytest"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -123,6 +125,8 @@ Letspan::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
+
 
 
 
