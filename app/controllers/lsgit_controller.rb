49099f7200b4c07f7322b5a5fb7e83112cd00e28
evanpurnama <evanpurnama5@gmail.com>
2012-12-19 01:01:25 +0800

    addNotifAjaxComet

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -63,22 +63,22 @@ class LsgitController < ApplicationController
 
   def deleteNotif 
     
-#    r = params[:path]
-#    #notif_id = params[:notif_id]
-#    apps_name = r.split("-__-")[1]
-#    @apps_name = apps_name
-#  #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
-#  #              Apps.where(:name => @apps_name).first.id, 
-#  #              User.where(:username => @username).first.id, 
-#  #              @username)
-#
-#    user_id = User.where(:username => @username).first.id
-#    apps_id = Apps.where(:name => @apps_name).first.id
-#
-#    nu = NotifsUsers.where(:user_id => user_id, :apps_id => apps_id)
-#    nu.destroy_all
-#
-#    return
+    r = params[:path]
+    #notif_id = params[:notif_id]
+    apps_name = r.split("-__-")[1]
+    @apps_name = apps_name
+  #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
+  #              Apps.where(:name => @apps_name).first.id, 
+  #              User.where(:username => @username).first.id, 
+  #              @username)
+
+    user_id = User.where(:username => @username).first.id
+    apps_id = Apps.where(:name => @apps_name).first.id
+
+    nu = NotifsUsers.where(:user_id => user_id, :apps_id => apps_id)
+    nu.destroy_all
+
+    return
     
   end
  
