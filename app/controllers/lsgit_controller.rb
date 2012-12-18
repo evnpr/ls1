bd5535730a0294182cbac38f8e865a47d4069389
evanpurnama <evanpurnama5@gmail.com>
2012-12-19 00:02:23 +0800

    addNotifAjaxComet

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -17,21 +17,6 @@ class LsgitController < ApplicationController
     apps_name = r.split("-__-")[1]
     @apps_name = apps_name
 
-    def event_happen(apps_name)
-        if !Apps.exists? (:name => apps_name)
-            return true
-        end
-        a = Apps.where(:name => apps_name).first
-        u = a.updateapp.first
-        if u.updated == 1
-            u.updated = 0
-            u.save
-            return false 
-        else
-            return true
-        end
-    end
-
   #  authenticate(Apps.where(:name => @apps_name).first.user.username, 
   #              Apps.where(:name => @apps_name).first.id, 
   #              User.where(:username => @username).first.id, 
