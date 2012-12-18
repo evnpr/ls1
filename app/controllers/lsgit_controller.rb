61ccfad4f7dc538cff3ab751ccf25bbe943e4007
evanpurnama <evanpurnama5@gmail.com>
2012-12-19 00:12:06 +0800

    addNotifAjaxComet

--- app/controllers/lsgit_controller.rb
+++ app/controllers/lsgit_controller.rb
@@ -19,9 +19,6 @@ class LsgitController < ApplicationController
 
     def event_happen(apps_name)
         a = Apps.where(:name => apps_name).first
-        if !Updateapp.exists?(:apps_id => a.id)
-            return true
-        end
         u = a.updateapp.first
         if u.updated == 1
             u.updated = 0
